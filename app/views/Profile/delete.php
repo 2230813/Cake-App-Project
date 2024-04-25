8a1ecd147fc788ca8b65f92d8bc292ce1100a109
Jodel Briones <jojos@jodels-air.home>
2024-04-24 20:58:04 -0400

    we now have profile deletion -_-

--- app/views/Profile/delete.php
+++ app/views/Profile/delete.php
@@ -10,8 +10,8 @@
     <div class="form-container">
         <h2>Delete Profile</h2>
         <p>Are you sure you want to delete your profile? This action cannot be undone.</p>
-        <form id="deleteProfileForm">
-            <button type="submit" class="btn btn-danger">Delete Profile</button>
+        <form id="deleteProfileForm" method='post' action=''>
+            <button type="submit" class="btn btn-danger" name="action">Delete Profile</button>
         </form>
     </div>
 </body>
