a936aceb4189d35d0dcc9daa7c7a64845101f38c
yuh89 <ayushp05@hotmail.com>
2024-05-02 19:41:49 -0400

    .

--- cakedb.sql
+++ cakedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2024 at 08:03 PM
+-- Generation Time: Apr 25, 2024 at 12:22 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,47 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `cakedb`
 --
-CREATE DATABASE IF NOT EXISTS `cakedb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `cakedb`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cart`
+-- Table structure for table `order`
 --
 
-DROP TABLE IF EXISTS `cart`;
-CREATE TABLE `cart` (
-  `cart_id` int(11) NOT NULL,
-  `profile_id` int(11) NOT NULL,
-  `total_price` decimal(4,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cartDetails`
---
-
-DROP TABLE IF EXISTS `cartDetails`;
-CREATE TABLE `cartDetails` (
-  `cart_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+CREATE TABLE `order` (
+  `orderID` int(11) NOT NULL,
   `date` date NOT NULL,
   `status` enum('Pending','Completed') NOT NULL,
-  `totalAmount` decimal(65,0) NOT NULL
+  `totalAmount` decimal(10,2) NOT NULL,
+  `profile_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderDetail`
+--
+
+CREATE TABLE `orderDetail` (
+  `orderDetail_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,14 +55,14 @@ CREATE TABLE `orders` (
 -- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
-  `price` decimal(4,2) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
   `details` text NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `image_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -85,7 +71,6 @@ CREATE TABLE `product` (
 -- Table structure for table `profile`
 --
 
-DROP TABLE IF EXISTS `profile`;
 CREATE TABLE `profile` (
   `profile_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -102,13 +87,11 @@ CREATE TABLE `profile` (
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password_hash` varchar(60) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT 1
+  `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -116,24 +99,19 @@ CREATE TABLE `user` (
 --
 
 --
--- Indexes for table `cart`
+-- Indexes for table `order`
 --
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cart_id`);
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `order_profileID_FK` (`profile_id`);
 
 --
--- Indexes for table `cartDetails`
+-- Indexes for table `orderDetail`
 --
-ALTER TABLE `cartDetails`
-  ADD KEY `CARTDETAILS_CART_ID_FK` (`cart_id`),
-  ADD KEY `CARTDETAILS_PRODUCT_ID_FK` (`product_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `ORDERS_USER_ID_FK` (`user_id`);
+ALTER TABLE `orderDetail`
+  ADD PRIMARY KEY (`orderDetail_id`),
+  ADD KEY `orderDetail_orderID_FK` (`order_id`),
+  ADD KEY `orderDetail_productID_FK` (`product_id`);
 
 --
 -- Indexes for table `product`
@@ -159,16 +137,16 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `cart`
+-- AUTO_INCREMENT for table `order`
 --
-ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `order`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `orderDetail`
 --
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orderDetail`
+  MODIFY `orderDetail_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -193,23 +171,23 @@ ALTER TABLE `user`
 --
 
 --
--- Constraints for table `cartDetails`
+-- Constraints for table `order`
 --
-ALTER TABLE `cartDetails`
-  ADD CONSTRAINT `CARTDETAILS_CART_ID_FK` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`),
-  ADD CONSTRAINT `CARTDETAILS_PRODUCT_ID_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_profileID_FK` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `orders`
+-- Constraints for table `orderDetail`
 --
-ALTER TABLE `orders`
-  ADD CONSTRAINT `ORDERS_USER_ID_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+ALTER TABLE `orderDetail`
+  ADD CONSTRAINT `orderDetail_orderID_FK` FOREIGN KEY (`order_id`) REFERENCES `order` (`orderID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `orderDetail_productID_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `profile`
 --
 ALTER TABLE `profile`
-  ADD CONSTRAINT `PROFILE_USER_ID_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `PROFILE_USER_ID_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
