ddc12ea9660b7256918c66c7e0dbf80fb55f9c23
yuh89 <ayushp05@hotmail.com>
2024-05-12 16:42:29 -0400

    ferfsewsfwsx

--- app/models/Order.php
+++ app/models/Order.php
@@ -8,27 +8,27 @@ Class Order extends \app\core\Model{
     public $date;
     public $status;
     public $totalAmount;
-    public $profile_id;//FK
+    public $user_id;//FK
 
 
     public function get($order_id) {
-        $SQL = "SELECT * FROM order WHERE orderID = :orderID";
+        $SQL = "SELECT * FROM orders WHERE orderID = :orderID";
         $STMT = self::$_conn->prepare($SQL);
         $STMT->execute(['orderID'=>$order_id]);
         $STMT->setFetchMode(\PDO::FETCH_CLASS,'app\models\Order');
         return $STMT->fetch();
     }
 
-    public function getAll($profile_id){
-        $SQL = "SELECT * FROM order WHERE profile_id = :profile_id";
+    public function getAll($user_id){
+        $SQL = "SELECT * FROM orders WHERE user_id = :user_id";
             $STMT = self::$_conn->prepare($SQL);
-            $STMT->execute(['profile_id'=>$profile_id]);
+            $STMT->execute(['user_id'=>$user_id]);
             $STMT->setFetchMode(\PDO::FETCH_CLASS,'app\models\Order');
             return $STMT->fetchAll();
     }
 
     public function cancelOrder($order_id){
-        $SQL = "DELETE FROM order WHERE order_id = :order_id";
+        $SQL = "DELETE FROM orders WHERE order_id = :order_id";
         $STMT = self::$_conn->prepare($SQL);
         $STMT->execute(["order_id" => $this->orderID]);
     }
