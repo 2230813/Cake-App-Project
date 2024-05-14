9ebab95261e54bf708e34dde16cd0436bcb1d257
yuh89 <ayushp05@hotmail.com>
2024-05-14 01:23:21 -0400

    dfsfdsfdsfdsfdsfsd

--- app/controllers/Order.php
+++ app/controllers/Order.php
@@ -7,22 +7,16 @@ class Order extends \app\core\Controller {
 
     //#[\app\filters\Login]
 
-    public function delete($order_id){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){ 
-			$order = new \app\models\Order();
-			$order = $order->get($order_id);
-			$order->cancelOrder($order_id);
-			header('location:/Order/order_list/' . $order->order_id);
+    //public function delete($order_id){
+        //if($_SERVER['REQUEST_METHOD'] === 'POST'){ 
+			//$order = new \app\models\Order();
+			//$order = $order->get($order_id);
+			//$order->cancelOrder($order_id);
+			//header('location:/Order/order_list/' . $order->order_id);
 			
-		}
-    }
+		//}
+    //}
 
-    public function getAllOrder($user_id){
-        $order = new \app\models\Order();
-		$order = $order->getAll($user_id);
-		
-        $this->view('Order/order_list',$order);
-    }
 
     public function seeOrder($cart_id){
         $order = new \app\models\Order();
--- app/models/Order.php
+++ app/models/Order.php
@@ -50,21 +50,4 @@ Class Order extends \app\core\Model{
         return $STMT->fetch();
     }
 
-    public function cancelOrder($order_id){
-        $SQL = "DELETE FROM orders WHERE order_id = :order_id";
-        $STMT = self::$_conn->prepare($SQL);
-        $STMT->execute(["order_id" => $this->orderID]);
-    }
-
-    public function createOrder(){
-        $SQL = 'INSERT INTO publication (profile_id,publication_title,publication_text,timestamp,publication_status) VALUES (:profile_id,:publication_title,:publication_text,:timestamp,:publication_status)';
-            $STMT = self::$_conn->prepare($SQL);
-            $STMT->execute([
-                'profile_id'=>$this->profile_id, 
-                'publication_title'=>$this->publication_title,
-                'publication_text'=>$this->publication_text,
-                'timestamp'=>$this->timestamp,
-                'publication_status'=>$this->publication_status]
-            );
-    }
 }
